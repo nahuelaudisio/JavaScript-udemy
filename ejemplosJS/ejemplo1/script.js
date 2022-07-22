9a5eee6970c268b9b721e098b5eeb5858a98eb4d
nahuelaudisio <nahuel.audisio.95@gmail.com>
2022-07-22 15:52:45 -0300

    segundo commit

---
+++ ejemplosJS/ejemplo1/script.js
@@ -0,0 +1,3 @@
+alert("hola desde el archivo script.js")
+console.warn("PELIGRO DE SABROSURA")
+console.log("SAPE DESDE EL BODY")
---
+++ ejemplosJS/ejemplo2/script.js
@@ -0,0 +1,10 @@
+
+//prompt("que edad tienes?",26)
+
+
+//alert("soy un alert!!!!");
+
+var edad = prompt("Que edad tienes?", 27);
+
+alert("hola, SOY GOKU y tu edad es -> " + edad + " AÑOS");
+console.log("hola criaturitas del señor");
