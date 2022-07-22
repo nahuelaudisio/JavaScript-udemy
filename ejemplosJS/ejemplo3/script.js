6e6ac9365395dc9548d808ae75bc842fef0a01ca
nahuelaudisio <nahuel.audisio.95@gmail.com>
2022-07-22 16:49:44 -0300

    tercer commit

---
+++ ejemplosJS/ejemplo3/script.js
@@ -0,0 +1,13 @@
+var edad = prompt("hola soy GOKU, que edad tienes?", 27);
+
+
+var mensaje = `La edad del sujeto es
+...
+... ${edad}` ;
+
+
+alert(mensaje);
+
+
+console.log("esto es un templatelog")
+
